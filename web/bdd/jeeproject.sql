dd1c9b4f1c3446211c7357da9e886259d88e182e
Florian Fasmeyer <florian.fasmeyer@he-arc.ch>
2018-04-25 12:47:03 +0200

    bdd

--- web/bdd/jeeproject.sql
+++ web/bdd/jeeproject.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.4
+-- version 4.7.4
 -- https://www.phpmyadmin.net/
 --
--- Client :  127.0.0.1
--- Généré le :  Jeu 12 Avril 2018 à 09:09
--- Version du serveur :  5.7.14
--- Version de PHP :  5.6.25
+-- Hôte : 127.0.0.1
+-- Généré le :  mer. 25 avr. 2018 à 12:44
+-- Version du serveur :  10.1.28-MariaDB
+-- Version de PHP :  7.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -63,6 +65,20 @@ CREATE TABLE `subject` (
   `parent_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Déchargement des données de la table `subject`
+--
+
+INSERT INTO `subject` (`subject_id`, `name`, `image`, `description`, `color`, `parent_id`) VALUES
+(1, 'Algèbre linéaire', '', 'Des maths comme il en faut!', '#ff745b', -1),
+(2, 'Physique', '', 'De la balistique à la quantique, tout y passe.', '#ff5b7c', -1),
+(3, 'Chimie', '', 'Make gold with silver!', '#ff5bcb', -1),
+(4, 'Matrices', '', 'Des matrices pour l\'amour de dieu', '#e553ff', -1),
+(5, 'Alchimie', '', 'Magic!', '#9253ff', -1),
+(6, 'Robotique', '', 'asdf', '#517fff', -1),
+(7, 'Illusion', '', 'lol', '#ff745b', -1),
+(8, 'Invocation', '', 'demonqiue', '#ff5b7c', -1);
+
 -- --------------------------------------------------------
 
 --
@@ -91,7 +107,7 @@ CREATE TABLE `user_groups` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Index pour les tables exportées
+-- Index pour les tables déchargées
 --
 
 --
@@ -125,7 +141,7 @@ ALTER TABLE `user_groups`
   ADD PRIMARY KEY (`usergroup_id`);
 
 --
--- AUTO_INCREMENT pour les tables exportées
+-- AUTO_INCREMENT pour les tables déchargées
 --
 
 --
@@ -133,26 +149,32 @@ ALTER TABLE `user_groups`
 --
 ALTER TABLE `article`
   MODIFY `article_id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT pour la table `comment`
 --
 ALTER TABLE `comment`
   MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT pour la table `subject`
 --
 ALTER TABLE `subject`
-  MODIFY `subject_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `subject_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
 --
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
   MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT pour la table `user_groups`
 --
 ALTER TABLE `user_groups`
   MODIFY `usergroup_id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
